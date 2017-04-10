0fd9ba9618e23c00efc068907a689c1bc93753ec
codonculous <feidameng01@gmail.com>
2017-04-09 21:16:51 -0400

    Configuring root as rover#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'rovers#index'
+  get 'rovers' => 'rovers#index'
+
   get 'rovers' => 'rovers#index'
 
   post 'rovers' => 'rovers#create'
